0428bf0a18ff0e1e9c1546feb257f43286b9cdc1
LcEduardo <lucasguima1704@gmail.com>
2024-02-02 15:00:04 -0300

    h2/p footer

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 function tempoDecorrido() {
     let mostraData = document.getElementById('data');
-
     let dataDeCriacao = new Date('2024-01-01');
-
     let dataAtual = new Date();
-
     let diferencaEmMilissegundos = dataAtual - dataDeCriacao;
-
     let diferencaEmDias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
-
     mostraData.innerHTML = `${diferencaEmDias}`;
 
     return diferencaEmDias;
+
 }
 
 function plantacao(){
-    var imagem = document.createElement('img');
+    let imagem = document.createElement('img');
     let container = document.getElementById('img');
+    let tempo = tempoDecorrido();
+
     imagem.width = 60;
     imagem.height = 60;
     imagem.style.transform = 'translate(+75%, +3%)';
-    let tempo = tempoDecorrido();
-
+    
     if( tempo <= 60){
         imagem.src = "imagens/planta-1.jpg";
         imagem.alt = 'desenho de uma semente de planta';
@@ -36,6 +33,7 @@ function plantacao(){
         imagem.src = "imagens/planta-3.jpg";
         imagem.alt = "desenho de uma planta grande";
         container.appendChild(imagem);
+
     }
 }
 
@@ -43,4 +41,5 @@ tempoDecorrido();
 
 document.addEventListener('DOMContentLoaded', function () {
     plantacao();
+    
 });
